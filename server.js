a44e9fd902461c5b63d164f061a37684e2b6ecbf
BarakBH <barakbhamu@gmail.com>
2023-05-07 14:59:05 +0300

    converting to proper mvc and modeling tours

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ mongoose.connect(DB, {
 }).then(() => console.log("DB connection successful!"));
 
 
-
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
